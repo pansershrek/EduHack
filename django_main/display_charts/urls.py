a9c402dc89fed656c8f4f4aa99793830f7628c56
Skiba Gleb <g.skiba@corp.mail.ru>
2020-06-20 03:59:50 +0300

    Add default value to charts

--- django_main/display_charts/urls.py
+++ django_main/display_charts/urls.py
@@ -4,7 +4,7 @@ from .views import line_chart, line_chart_json
 from django.shortcuts import render
 
 
-def test_touch(request):
+def test_touch(request, id):
     return HttpResponse("Ok")
 
 
@@ -17,10 +17,31 @@ def kek(request):
     return render(request, "programms_list.html", {"programms": [{"id": "1", "name": "kek"}, ]})
 
 
+def lel(request):
+    return render(
+        request, "chart_slices.html",
+        {
+            "criteria": {
+                "name": "NAME", "slicename": "SLICENAME",
+                "slices": [
+                    {"id": "1", "name": "name1"},
+                    {"id": "2", "name": "name2"}
+                ],
+                "charts": [
+                    {"id": 1, "data": [[1, 2, 3], [1, 2, 3]]},
+                    {"id": 2, "data": [[1, 2, 3], [1, 2, 3]]}
+                ]
+            },
+
+        }
+    )
+
 urlpatterns = [
     path('', test_touch, name='Test touch'),
     path('chart', line_chart, name='line_chart'),
     path('chartJSON', line_chart_json, name='line_chart_json'),
     path('kek', kek, name="kek"),
-    path('programm/<int:id>', test_touch, name="programm")
+    path('lel', lel, name="lel"),
+    path('programm/<int:id>', test_touch, name="programm"),
+    path('slice/<int:id>', test_touch, name="slice")
 ]
